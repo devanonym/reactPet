d6c7c68958122d19d9bbccee4d7ef415742a264a
Rozanov Evgeny <evgeniy_rozanov@sofvers.com>
2018-05-10 16:43:08 +0400

    close sidebar after logout

--- src/main/webapp/app/modules/login/logout.tsx
+++ src/main/webapp/app/modules/login/logout.tsx
@@ -3,14 +3,17 @@ import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
 import { logout } from 'app/shared/reducers/authentication';
+import { sideMenuClose } from 'app/shared/layout/sidebar/sidebar.reducer';
 
 export interface ILogoutProps {
   logout: Function;
+  sideMenuClose: Function;
 }
 
 export class Logout extends React.Component<ILogoutProps> {
   componentWillMount() {
     this.props.logout();
+    this.props.sideMenuClose();
   }
 
   render() {
@@ -29,6 +32,6 @@ export class Logout extends React.Component<ILogoutProps> {
 
 const mapStateToProps = storeState => ({});
 
-const mapDispatchToProps = { logout };
+const mapDispatchToProps = { logout, sideMenuClose };
 
 export default connect(mapStateToProps, mapDispatchToProps)(Logout);
